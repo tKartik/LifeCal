2bc613b719c86aaa17b1dde804877e4394d01bef
Kartik <tyagik2097@gmail.com>
2019-08-23 04:28:12 +0530

    Updates

---
+++ precache-manifest.5a442e55053611cfa019a032b6804369.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452b1ed64916ec6577361e6e879ca233",
+    "url": "/LifeCal/index.html"
+  },
+  {
+    "revision": "f0a56f3a286e2fdf1e78",
+    "url": "/LifeCal/static/css/main.a085a1f7.chunk.css"
+  },
+  {
+    "revision": "ea01f6719d92d9af5a2a",
+    "url": "/LifeCal/static/js/2.bfb391e6.chunk.js"
+  },
+  {
+    "revision": "f0a56f3a286e2fdf1e78",
+    "url": "/LifeCal/static/js/main.029270cd.chunk.js"
+  },
+  {
+    "revision": "b38885b5bca2fa63a107",
+    "url": "/LifeCal/static/js/runtime~main.4aceaac4.js"
+  }
+]);
